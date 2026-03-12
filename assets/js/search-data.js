080a6a6c131d84c88f9d39c98dad5c62bd2fa048
MeggieMarschner <150011209+MeggieMarschner@users.noreply.github.com>
2026-03-12 10:19:35 +0000

    Deploying to gh-pages from @ MeggieMarschner/MeggieMarschner.github.io@b9fa0d68c1f5f88f36dac9c324f3b828e08fe804 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -499,30 +499,4 @@ ninja.data = [{
         handler: () => {
           window.open("https://orcid.org/0009-0000-7741-2298", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
